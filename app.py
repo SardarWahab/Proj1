4296b392eafbdeccfe8b2cc43d52e5a871f053b8
SardarWahab <wahabsardar130@gmail.com>
2024-11-05 16:06:24 +0500

    "Updated app.py to use render_template and added new routes for about and contact pages"

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 # save this as app.py
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    return render_template('index.html')
+
+
+@app.route("/about")
+def about():
+    return render_template('index.html')
+
+
+@app.route("/contact")
+def contact():
+    return render_template('index.html')
