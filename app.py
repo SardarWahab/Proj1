16f11e085076bce88e1a9f5ccd7e6b4bd47b03a3
SardarWahab <wahabsardar130@gmail.com>
2024-11-06 15:56:00 +0500

    "Added Flask-SQLAlchemy and Flask-Migrate to app.py, created User and Subject database models, and updated index.html with a new background image."

--- app.py
+++ app.py
@@ -1,8 +1,30 @@
 # save this as app.py
 from flask import Flask, render_template
-
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 app = Flask(__name__)
+# adding configuration for using a sqlite database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False  
 
+# Creating an SQLAlchemy instance
+db = SQLAlchemy(app)
+migrate = Migrate(app,db)
+
+
+#database classes
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(20), nullable=False)
+
+
+
+class Subject(db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        subject_name = db.Column(db.String(20), nullable=False)
+        subject_code = db.Column(db.String(20), nullable=False)
 @app.route("/")
 def hello():
     return render_template('index.html')
